5aeaab746cbd3f3966a3ce7e5cf62a4b91a48ebb
yeshenggen <1617683532@qq.com>
2022-05-18 22:52:33 +0800

    商品编辑

--- lib/ui/ware_edit.dart
+++ lib/ui/ware_edit.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutterqwb/utils/color_util.dart';
-import 'package:flutterqwb/utils/color_util.dart';
 
 import '../utils/font_size_util.dart';
 
-class WareEdit extends StatelessWidget {
+class WareEdit extends StatefulWidget {
+
   const WareEdit({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return WareEditState();
+  }
+}
+
+class WareEditState extends State<WareEdit>{
   @override
   Widget build(BuildContext context) {
-    ModalRoute.of(context)?.settings.arguments;
-
     return Scaffold(
       appBar: AppBar(
           leading: IconButton(
@@ -22,7 +28,7 @@ class WareEdit extends StatelessWidget {
       bottomNavigationBar: Container(
         margin: const EdgeInsets.only(left: 10, right: 10),
         child: RaisedButton(
-            onPressed: () {},
+            onPressed: _save,
             child: const Text("保存"),
             textColor: Colors.white,
             color: Colors.blue),
@@ -39,10 +45,12 @@ class WareEdit extends StatelessWidget {
                   Text("商品总类:", style: TextStyle(color: ColorUtil.GRAY_6)),
                   Expanded(
                       child: GestureDetector(
-                    onTap: () {},
-                    child:
-                        Text("库存商品类", style: TextStyle(color: ColorUtil.BLUE)),
-                  )),
+                        onTap: (){
+                          _showDialogWareIsType(context);
+                        },
+                        child:
+                        Text(_isTypeText, style: TextStyle(color: ColorUtil.BLUE)),
+                      )),
                   const Icon(Icons.arrow_drop_down)
                 ],
               ),
@@ -56,10 +64,12 @@ class WareEdit extends StatelessWidget {
               child: Row(
                 children: [
                   Text("商品类别属性:", style: TextStyle(color: ColorUtil.GRAY_6)),
-                  Radio(value: 0, groupValue: [0, 1], onChanged: (value){}),
-                  Text("实物商品"),
-                  Radio(value: 1, groupValue: [0, 1], onChanged: (value){}),
-                  Text("服务商品"),
+//                  Radio(value: 0, groupValue: _radio, onChanged: (value)=> _changeRadioValue(value)),
+                  Radio(value: 0, groupValue: _radio, onChanged: null),
+                  Text("实物商品", style: TextStyle(color: ColorUtil.GRAY_6, fontSize: FontSizeUtil.MIDDLE)),
+                  Radio(value: 1, groupValue: _radio, onChanged: null),
+                  Radio(value: 1, groupValue: _radio, onChanged: (value)=>_changeRadioValue(value)),
+                  Text("服务商品", style: TextStyle(color: ColorUtil.GRAY_6, fontSize: FontSizeUtil.MIDDLE)),
                 ],
               ),
             ),
@@ -93,13 +103,15 @@ class WareEdit extends StatelessWidget {
             ),
             SizedBox(
               child: Row(
-                children: const [
-                  Text("商品名称:"),
-                  Expanded(child: TextField(decoration:InputDecoration(
-                      hintText: "请输入商品名称",
-                      hintStyle: TextStyle(color: Color(0xFF999999), fontSize: 13),
-                      border: OutlineInputBorder(borderSide: BorderSide.none)
-                  ))),
+                children: [
+                  Text("商品名称:", style: TextStyle(color: ColorUtil.GRAY_6, fontSize: FontSizeUtil.MIDDLE),),
+                  Expanded(child: TextField(
+                    controller: _wareNameController,
+                      decoration:const InputDecoration(
+                        hintText: "请输入商品名称",
+                        hintStyle: TextStyle(color: Color(0xFF999999), fontSize: 13),
+                        border: OutlineInputBorder(borderSide: BorderSide.none)
+                    ))),
                 ],
               ),
             ),
@@ -108,10 +120,11 @@ class WareEdit extends StatelessWidget {
               child: Row(
                 children: [
                   Expanded(child: Row(
-                    children: const [
-                      Text("单位(大):"),
+                    children: [
+                      Text("单位(大):", style: TextStyle(color: ColorUtil.GRAY_6, fontSize: FontSizeUtil.MIDDLE)),
                       Expanded(child: TextField(
-                        decoration: InputDecoration(
+                        controller: _maxUnitController,
+                        decoration: const InputDecoration(
                           hintText: "如箱",
                           hintStyle: TextStyle(color: Color(0xFF999999), fontSize: 12),
                           border: OutlineInputBorder(borderSide: BorderSide.none),
@@ -120,10 +133,11 @@ class WareEdit extends StatelessWidget {
                     ],
                   )),
                   Expanded(child: Row(
-                    children: const [
-                      Text("单位(小):"),
+                    children: [
+                      Text("单位(小):", style: TextStyle(color: ColorUtil.GRAY_6, fontSize: FontSizeUtil.MIDDLE)),
                       Expanded(child: TextField(
-                        decoration: InputDecoration(
+                        controller: _minUnitController,
+                        decoration: const InputDecoration(
                           hintText: "如瓶",
                           hintStyle: TextStyle(color: Color(0xFf999999), fontSize: 12),
                           border: OutlineInputBorder(
@@ -141,10 +155,11 @@ class WareEdit extends StatelessWidget {
               child: Row(
                 children: [
                   Expanded(child: Row(
-                    children: const [
-                      Text("规格(大):", style: TextStyle(color: Color(0xFF666666), fontSize: 13),),
+                    children: [
+                      Text("规格(大):", style: TextStyle(color: ColorUtil.GRAY_6, fontSize: FontSizeUtil.MIDDLE),),
                       Expanded(child: TextField(
-                        decoration: InputDecoration(
+                        controller: _maxWareGgUnitController,
+                        decoration: const InputDecoration(
                             hintText: "如500ml*6",
                             hintStyle: TextStyle(color: Color(0xff999999), fontSize: 13),
                             border: OutlineInputBorder(
@@ -155,10 +170,11 @@ class WareEdit extends StatelessWidget {
                     ],
                   )),
                   Expanded(child: Row(
-                    children: const [
-                      Text("规格(小):", style: TextStyle(color: Color(0xff666666), fontSize: 13),),
+                    children: [
+                      Text("规格(小):", style: TextStyle(color: ColorUtil.GRAY_6, fontSize: FontSizeUtil.MIDDLE),),
                       Expanded(child: TextField(
-                        decoration: InputDecoration(
+                        controller: _minWareGgUnitController,
+                        decoration: const InputDecoration(
                             hintText: "如500ml",
                             hintStyle: TextStyle(color: Color(0xff999999), fontSize: 13),
                             border: OutlineInputBorder(
@@ -179,12 +195,13 @@ class WareEdit extends StatelessWidget {
                     children: [
                       Text("条码(大):", style: TextStyle(color: ColorUtil.GRAY_6, fontSize: FontSizeUtil.MIDDLE),),
                       Expanded(child: TextField(
+                        controller: _maxBarCodeController,
                         decoration: InputDecoration(
-                          hintText: "如(箱码)",
-                          hintStyle: TextStyle(color: ColorUtil.GRAY_9, fontSize: FontSizeUtil.MIDDLE),
-                          border: const OutlineInputBorder(
-                            borderSide: BorderSide.none
-                          )
+                            hintText: "如(箱码)",
+                            hintStyle: TextStyle(color: ColorUtil.GRAY_9, fontSize: FontSizeUtil.MIDDLE),
+                            border: const OutlineInputBorder(
+                                borderSide: BorderSide.none
+                            )
                         ),
                       )),
                       IconButton(onPressed: (){}, icon: const Icon(Icons.scanner))
@@ -194,12 +211,13 @@ class WareEdit extends StatelessWidget {
                     children: [
                       Text("条码(小):", style: TextStyle(color: ColorUtil.GRAY_6, fontSize: FontSizeUtil.MIDDLE)),
                       Expanded(child: TextField(
+                        controller: _minBarCodeController,
                         decoration: InputDecoration(
-                          hintText: "如(瓶码)",
-                          hintStyle: TextStyle(color: ColorUtil.GRAY_9, fontSize: FontSizeUtil.MIDDLE),
-                          border: const OutlineInputBorder(
-                            borderSide: BorderSide.none
-                          )
+                            hintText: "如(瓶码)",
+                            hintStyle: TextStyle(color: ColorUtil.GRAY_9, fontSize: FontSizeUtil.MIDDLE),
+                            border: const OutlineInputBorder(
+                                borderSide: BorderSide.none
+                            )
                         ),
                       )),
                       IconButton(onPressed: (){}, icon: const Icon(Icons.scanner))
@@ -216,12 +234,13 @@ class WareEdit extends StatelessWidget {
                   Text("1", style: TextStyle(color: ColorUtil.GRAY_3, fontSize: FontSizeUtil.BIG)),
                   Text("*大单位=", style: TextStyle(color: ColorUtil.GRAY_6, fontSize: FontSizeUtil.MIDDLE)),
                   Expanded(child: TextField(
+                    controller: _sUnitController,
                     decoration: InputDecoration(
-                      hintText: "点击输入",
-                      hintStyle: TextStyle(color: ColorUtil.GRAY_9, fontSize: FontSizeUtil.MIDDLE),
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide.none
-                      )
+                        hintText: "点击输入",
+                        hintStyle: TextStyle(color: ColorUtil.GRAY_9, fontSize: FontSizeUtil.MIDDLE),
+                        border:  const OutlineInputBorder(
+                            borderSide: BorderSide.none
+                        )
                     ),
                   )),
                   Text("*小单位", style: TextStyle(color: ColorUtil.GRAY_6, fontSize: FontSizeUtil.MIDDLE)),
@@ -242,11 +261,11 @@ class WareEdit extends StatelessWidget {
                       SizedBox(width:40,child: TextButton(onPressed: (){}, child: Text("选择", style: TextStyle(color: ColorUtil.BLUE, fontSize: FontSizeUtil.MIDDLE))),),
                       Expanded(child: TextField(
                         decoration: InputDecoration(
-                          hintText: "点击输入",
-                          hintStyle: TextStyle(color: ColorUtil.GRAY_9, fontSize: FontSizeUtil.MIDDLE),
-                          border: const OutlineInputBorder(
-                            borderSide: BorderSide.none
-                          )
+                            hintText: "点击输入",
+                            hintStyle: TextStyle(color: ColorUtil.GRAY_9, fontSize: FontSizeUtil.MIDDLE),
+                            border: const OutlineInputBorder(
+                                borderSide: BorderSide.none
+                            )
                         ),
                       ))
                     ],
@@ -472,11 +491,11 @@ class WareEdit extends StatelessWidget {
                       Text("保质期:", style: TextStyle(color: ColorUtil.GRAY_6, fontSize: FontSizeUtil.MIDDLE)),
                       Expanded(child: TextField(
                         decoration: InputDecoration(
-                          hintText: "点击输入",
-                          hintStyle: TextStyle(color: ColorUtil.GRAY_9, fontSize: FontSizeUtil.MIDDLE),
-                          border: const OutlineInputBorder(
-                            borderSide: BorderSide.none
-                          )
+                            hintText: "点击输入",
+                            hintStyle: TextStyle(color: ColorUtil.GRAY_9, fontSize: FontSizeUtil.MIDDLE),
+                            border: const OutlineInputBorder(
+                                borderSide: BorderSide.none
+                            )
                         ),
                       )),
                       TextButton(onPressed: (){}, child: Text("天", style: TextStyle(color: ColorUtil.BLUE, fontSize: FontSizeUtil.MIDDLE),),)
@@ -487,11 +506,11 @@ class WareEdit extends StatelessWidget {
                       Text("保质期预警:", style: TextStyle(color: ColorUtil.GRAY_6, fontSize: FontSizeUtil.MIDDLE)),
                       Expanded(child: TextField(
                         decoration: InputDecoration(
-                          hintText: "点击输入",
-                          hintStyle: TextStyle(color: ColorUtil.GRAY_9, fontSize: FontSizeUtil.MIDDLE),
-                          border: const OutlineInputBorder(
-                            borderSide: BorderSide.none
-                          )
+                            hintText: "点击输入",
+                            hintStyle: TextStyle(color: ColorUtil.GRAY_9, fontSize: FontSizeUtil.MIDDLE),
+                            border: const OutlineInputBorder(
+                                borderSide: BorderSide.none
+                            )
                         ),
                       )),
                       Text("天",style: TextStyle(color: ColorUtil.GRAY_6, fontSize: FontSizeUtil.MIDDLE))
@@ -528,9 +547,9 @@ class WareEdit extends StatelessWidget {
                       Text("预警最低数量:", style: TextStyle(color: ColorUtil.GRAY_6, fontSize: FontSizeUtil.MIDDLE)),
                       Expanded(child: TextField(
                         decoration: InputDecoration(
-                          hintText: "点击输入",
-                          hintStyle: TextStyle(color: ColorUtil.GRAY_9, fontSize: FontSizeUtil.MIDDLE),
-                          border: const OutlineInputBorder(borderSide: BorderSide.none)
+                            hintText: "点击输入",
+                            hintStyle: TextStyle(color: ColorUtil.GRAY_9, fontSize: FontSizeUtil.MIDDLE),
+                            border: const OutlineInputBorder(borderSide: BorderSide.none)
                         ),
                       ))
                     ],
@@ -549,4 +568,83 @@ class WareEdit extends StatelessWidget {
       ),
     );
   }
+
+  int _radio = 0;
+  int _isTypeValue = 1;
+  String _isTypeText = "库存商品类";
+  TextEditingController _wareNameController = TextEditingController(text: "青岛打油");
+  TextEditingController _maxUnitController = TextEditingController();
+  TextEditingController _minUnitController = TextEditingController();
+  TextEditingController _maxWareGgUnitController = TextEditingController();
+  TextEditingController _minWareGgUnitController = TextEditingController();
+  TextEditingController _maxBarCodeController = TextEditingController();
+  TextEditingController _minBarCodeController = TextEditingController();
+  TextEditingController _sUnitController = TextEditingController();
+
+  void _showDialogWareIsType(BuildContext context){
+    showDialog(
+      context: context,
+      builder: (_)=> SimpleDialog(
+        title: const Text("选择"),
+        children: [
+          SimpleDialogOption(
+            child: const Text("库存商品类"),
+            onPressed: (){
+              _changeWareIsType(1, "库存商品类");
+            },
+          ),
+          SimpleDialogOption(
+            child: const Text("原辅材料类"),
+            onPressed: (){
+              _changeWareIsType(2, "原辅材料类");
+            },
+          ),
+          SimpleDialogOption(
+            child: const Text("低值易耗品类"),
+            onPressed: (){
+              _changeWareIsType(3, "低值易耗品类");
+            },
+          ),
+          SimpleDialogOption(
+            child: const Text("固定资产类"),
+            onPressed: (){
+              _changeWareIsType(4, "固定资产类");
+            },
+          ),
+          SimpleDialogOption(
+            child: const Text("联盟商品类"),
+            onPressed: (){
+              _changeWareIsType(5, "联盟商品类");
+            },
+          ),
+        ],
+      )
+    );
+  }
+
+  void _changeWareIsType(value, text){
+    _isTypeValue = value;
+    Navigator.pop(context);
+    setState(() {
+      _isTypeText = text;
+    });
+  }
+
+  void _changeRadioValue(value){
+    setState(() {
+      _radio = value;
+    });
+  }
+
+  void _save(){
+    String wareName = _wareNameController.text;
+    String maxUnit = _maxUnitController.text;
+    String minUnit = _minUnitController.text;
+    String maxWareGg = _maxWareGgUnitController.text;
+    String minWareGg = _minWareGgUnitController.text;
+    String maxBarCode = _maxBarCodeController.text;
+    String minBarCode = _minBarCodeController.text;
+    String sUnit = _sUnitController.text;
+  }
+
 }
